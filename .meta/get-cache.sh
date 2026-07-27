12ed4fab786014dec4f82d1f35c54b27490d1760
Joscha <joscha@plugh.de>
2026-07-27 18:58:18 +0200

    ci: include toolchain in scope

--- .meta/get-cache.sh
+++ .meta/get-cache.sh
@@ -4,8 +4,11 @@ META_DIR=$(dirname "$(realpath "$0")")
 ROOT_DIR=$(dirname "$META_DIR")
 export LAKE_CONFIG="$META_DIR/lake-config.toml"
 
+TOOLCHAIN="$(cat "$ROOT_DIR/lean-toolchain")"
+TOOLCHAIN="${TOOLCHAIN#leanprover/lean4:}"
+
 python3 "$ROOT_DIR/.downstream/list.py" "$ROOT_DIR" | while IFS= read -r repo; do
   pushd "$ROOT_DIR/$repo"
-  lake cache get --scope "$repo" || true
+  lake cache get --scope "$TOOLCHAIN/$repo" || true
   popd
 done
