fffdef24ccc258dc09a59c83370504d84c430ac2
Joscha <joscha@plugh.de>
2026-07-30 16:57:56 +0200

    ci: always use full toolchain for cache scope

--- .meta/get-cache.sh
+++ .meta/get-cache.sh
@@ -5,8 +5,6 @@ ROOT_DIR=$(dirname "$META_DIR")
 export LAKE_CONFIG="$META_DIR/lake-config.toml"
 
 TOOLCHAIN="$(cat "$ROOT_DIR/lean-toolchain")"
-TOOLCHAIN="${TOOLCHAIN#leanprover/lean4:}"
-
 "$ROOT_DIR/.downstream/list.py" "$ROOT_DIR" -d -- "$@" | while IFS= read -r repo; do
   pushd "$ROOT_DIR/$repo"
   lake cache get --scope "$TOOLCHAIN/$repo" || true
