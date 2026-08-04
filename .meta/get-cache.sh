5fd904ae1077e42dea829fd4bb83aebe0a0ed3a3
Joscha <joscha@plugh.de>
2026-08-04 22:43:03 +0200

    fix: getting cache in older python versions
    See also https://github.com/python/cpython/issues/59317

--- .meta/get-cache.sh
+++ .meta/get-cache.sh
@@ -5,7 +5,7 @@ ROOT_DIR=$(dirname "$META_DIR")
 export LAKE_CONFIG="$META_DIR/lake-config.toml"
 
 TOOLCHAIN="$(cat "$ROOT_DIR/lean-toolchain")"
-"$ROOT_DIR/.downstream/list.py" "$ROOT_DIR" -d -- "$@" | while IFS= read -r repo; do
+"$ROOT_DIR/.downstream/list.py" -d -- "$ROOT_DIR" "$@" | while IFS= read -r repo; do
   pushd "$ROOT_DIR/$repo"
   lake cache get --scope "$TOOLCHAIN/$repo" || true
   popd
