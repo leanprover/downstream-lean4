47640670cb7cee5541bbdcd567c8441f42de7d86
Rob Simmons <rob@lean-fro.org>
2026-08-19 20:47:16 +0000

    use asNode/mkNode to avoid previewing trie implementation details

--- batteries/Batteries/Lean/Meta/DiscrTree.lean
+++ batteries/Batteries/Lean/Meta/DiscrTree.lean
@@ -38,18 +38,10 @@ namespace Trie
 /--
 Merge two `Trie`s. Duplicate values are preserved.
 -/
-partial def mergePreservingDuplicates : Trie α → Trie α → Trie α
-  | chain k₁ c₁, chain k₂ c₂ =>
-    if k₁ == k₂ then
-      .chain k₁ (mergePreservingDuplicates c₁ c₂)
-    else
-      node #[] (mergeChildren #[(k₁, c₁)] #[(k₂, c₂)])
-  | chain k₁ c₁, node vs₂ cs₂ =>
-    node vs₂ (mergeChildren #[(k₁, c₁)] cs₂)
-  | node vs₁ cs₁, chain k₂ c₂ =>
-    node vs₁ (mergeChildren cs₁ #[(k₂, c₂)])
-  | node vs₁ cs₁, node vs₂ cs₂ =>
-    node (vs₁ ++ vs₂) (mergeChildren cs₁ cs₂)
+partial def mergePreservingDuplicates (t₁ t₂ : Trie α) : Trie α :=
+  match (t₁.asNode, t₂.asNode) with
+  | ((vs₁, cs₁), (vs₂, cs₂)) =>
+    Trie.mkNode (vs₁ ++ vs₂) (mergeChildren cs₁ cs₂)
 where
   /-- Auxiliary definition for `mergePreservingDuplicates`. -/
   mergeChildren (cs₁ cs₂ : Array (Key × Trie α)) :
