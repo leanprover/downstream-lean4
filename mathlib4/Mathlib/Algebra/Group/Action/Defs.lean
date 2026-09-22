f2674d00742023ea6cc6972da203b62459e2456c
Kim Morrison <kim@lean-fro.org>
2026-09-22 21:11:06 +1000

    chore: drop mathlib4 instSMulOfMul priority override, obsoleted by lean4#13554

--- mathlib4/Mathlib/Algebra/Group/Action/Defs.lean
+++ mathlib4/Mathlib/Algebra/Group/Action/Defs.lean
@@ -54,13 +54,10 @@ open Function (Injective Surjective)
 
 variable {M N G H α β γ δ : Type*}
 
--- Note that https://github.com/leanprover/lean4/pull/13554
--- also makes the instance priority change, so if that is merged then `instance 1100` can
--- be removed here (we still want `to_additive` though).
-
--- see Note [higher instance priority]
+-- `instSMulOfMul` carries priority 1100 in core, and `to_additive` copies that priority
+-- over to `instVAddOfAdd`.
 /- See also `Monoid.toMulAction` and `MulZeroClass.toSMulWithZero`. -/
-attribute [instance 1100, to_additive /-- See also `AddMonoid.toAddAction` -/] instSMulOfMul
+attribute [to_additive /-- See also `AddMonoid.toAddAction` -/] instSMulOfMul
 
 /-- Like `Mul.toSMul`, but multiplies on the right.
 
